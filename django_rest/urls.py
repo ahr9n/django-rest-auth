6a3c8c17558f63e3d106829debe33a2f67b022ab
Ahmad Abdulrahman <ah.abdulrahmaan@gmail.com>
2022-12-05 00:37:43 +0200

    Fix path name

--- django_rest/urls.py
+++ django_rest/urls.py
@@ -3,7 +3,7 @@ from knox import views as knox_views
 from django.urls import path
 
 urlpatterns = [
-    path("register/", RegisterAPI.as_view()),
+    path("registration/", RegisterAPI.as_view()),
     path("login/", LoginAPI.as_view()),
     path("logout/", knox_views.LogoutView.as_view(), name="knox_logout"),
 ]
